09294d62eaf0778858dd3c9a55b1f3caec56e138
nabbisen <nabbisen@scqr.net>
2024-06-13 12:20:23 +0900

    leptos trial w/ inner_text (innerText)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +10,31 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // let s = "Wow This\nis perhaps a\ncat.\n".split("\n").collect::<Vec<&str>>();
+    // let t = "This\nis not a\ncat.\nOh...\n".split("\n").collect::<Vec<&str>>();
+    // let d = similar::TextDiff::configure().diff_slices(s.as_slice(), t.as_slice());
+    // d.ops().iter().for_each(|x| println!("{:?}|{}|{}", x.tag(), s[x.old_range()].join("\n"), t[x.new_range()].join("\n")));
+
+    // println!("{:?}", d.ops()  (|x| println!("{:?}", x.tag())));
+    
+    // d.iter_all_changes().into_iter().for_each(|x| println!("{:?}", x));
+    // let diff = similar::TextDiff::configure().diff_lines(S, T);
+    // print!("{}", diff.unified_diff()
+    // .header("old_file", "new_file"));
+
     tauri::Builder::default()
+        
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.set_zoom(1.20).unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
+
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,11 @@
-use leptos::leptos_dom::ev::SubmitEvent;
 use leptos::*;
 use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
+
+use similar::DiffTag;
+use std::ops::Range;
+
+use crate::ui::diff_block::DiffBlock;
 
 #[wasm_bindgen]
 extern "C" {
@@ -15,54 +18,68 @@ struct GreetArgs<'a> {
     name: &'a str,
 }
 
+#[derive(Clone, Eq, Hash)]
+struct DiffBlockOp {
+    tag: DiffTag,
+    old: Range<usize>,
+    new: Range<usize>,
+}
+impl PartialEq for DiffBlockOp {
+    fn eq(&self, other: &Self) -> bool {
+        self.tag == other.tag && self.old == other.old && self.new == other.new
+    }
+}
+
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = create_signal(String::new());
-    let (greet_msg, set_greet_msg) = create_signal(String::new());
+    // const S: &str = "Wow This\nis perhaps a\ncat.\n";
+    // const T: &str = "This\nis not a\ncat.\nOh...\n";
+    // let diff_words_opts = TextDiff::configure().diff_chars(S, T).ops().to_owned();
+    // let diff_words_opts2 = diff_words_opts.clone();
+    const S: &str = "Wow This\n\nis perhaps a\ncat.\nNow time\nto go\noutside ?\n";
+    const T: &str = "This\nis not a\ncat.\nOh...\nNow time\nto go\nabroad!!\nYay\n\n";
+    let s = S.split("\n").collect::<Vec<&str>>();
+    let t = T.split("\n").collect::<Vec<&str>>();
+    let d = similar::TextDiff::configure().diff_slices(s.as_slice(), t.as_slice());
+    let ops: Vec<DiffBlockOp> = d.ops().iter().map(|x| DiffBlockOp{ tag: x.tag(), old: x.old_range(), new: x.new_range()}).collect::<Vec<_>>();
+    // d.ops().iter().for_each(|x| println!("{:?}|{}|{}", x.tag(), s[x.old_range()].join("\n"), t[x.new_range()].join("\n")));
 
-    let update_name = move |ev| {
-        let v = event_target_value(&ev);
-        set_name.set(v);
-    };
+    // use web_sys::console;
+    // let t = similar::utils::diff_lines(similar::Algorithm::Myers, S, T).
+    // console::log_1
 
-    let greet = move |ev: SubmitEvent| {
-        ev.prevent_default();
-        spawn_local(async move {
-            let name = name.get_untracked();
-            if name.is_empty() {
-                return;
-            }
-
-            let args = to_value(&GreetArgs { name: &name }).unwrap();
-            // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
-        });
-    };
+    // let (ops, set_ops) = create_signal(Vec::<DiffBlockOp>::new());
+    let (ops, set_ops) = create_signal(ops);
 
     view! {
         <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-                </a>
-            </div>
+            <div style="width: 96vw;">
+                <For
+                    each=move || ops.get().into_iter().enumerate()
+                    key=|n| n.clone()
+                    children=move |(n, x)| {
+                        let source = s[x.old].iter().map(|x| x.to_string()).collect::<Vec<String>>();
+                        let target = t[x.new].iter().map(|x| x.to_string()).collect::<Vec<String>>();
 
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
-
-            <form class="row" on:submit=greet>
-                <input
-                    id="greet-input"
-                    placeholder="Enter a name..."
-                    on:input=update_name
+                        view! {
+                            <div style="display: flex;" tabindex="-1" class={ format!("diff diff-{}", n) }>
+                                <div style="display: block; width: 48vw;" tabindex="-1">
+                                    <DiffBlock diff_tag=x.tag source=source.clone() target=target.clone() is_target=false on_input={move |ev: leptos::ev::Event| {
+                                        let target = ev.target().unwrap();
+                                        let target: &web_sys::HtmlElement = target.dyn_ref().unwrap();                                    target.inner_text();
+                                        // todo use innerText
+                                        let _inner_text = target.inner_text();
+                                        set_ops.set(ops.get_untracked().iter().map(|x| if x.tag != DiffTag::Equal {x.to_owned()} else {DiffBlockOp {tag: DiffTag::Replace, old: x.old.clone(), new: x.new.clone()} } ).collect::<Vec<DiffBlockOp>>());
+                                    }} tab_index=n />
+                                </div>
+                                <div style="display: block; width: 48vw;" tabindex="-1">
+                                    <DiffBlock diff_tag=x.tag source=source.clone() target=target.clone() is_target=true on_input=move |_| { println!("test"); }  tab_index=1000 + n />
+                                </div>
+                            </div>
+                        }
+                    }
                 />
-                <button type="submit">"Greet"</button>
-            </form>
-
-            <p><b>{ move || greet_msg.get() }</b></p>
+            </div>
         </main>
     }
 }
