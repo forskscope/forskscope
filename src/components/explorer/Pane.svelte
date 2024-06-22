b2312ab506c2026053a707c54c34814eb636c275
nabbisen <nabbisen@scqr.net>
2024-06-23 08:42:11 +0900

    explorer pane as component

---
+++ src/components/explorer/Pane.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+  import { onMount, createEventDispatcher } from 'svelte';
+  
+  export let filter: string
+
+  const dispatch = createEventDispatcher();
+
+  let dirs: string[] = []
+  let files: string[] = []
+
+  let dir: string = ''
+  let filename: string = ''
+
+  let selected: string | undefined
+  function handleSelected(event: Event & { currentTarget: EventTarget & HTMLInputElement; }) {
+    filename = event.currentTarget.value
+    dispatch('selectedChange', { dir: dir, filename: filename })
+  }
+
+  async function update(subdir: string) {
+    dir = dir ? `${dir}/${subdir}` : ''
+    filename = ''
+    dispatch('selectedChange', { dir: dir, filename: filename })
+    
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    const ret = await invoke("list_dir", { currentDir: dir }) as {current_dir: string, dirs: string[], files: string[]}
+
+    dir = ret.current_dir
+    dirs = ret.dirs
+    files = ret.files
+  }
+
+  async function ready() {
+    await update('')
+  }
+
+  onMount(ready)
+</script>
+
+<div>
+  <span style="color: grey;">{dir}</span>
+  /
+  <span style="color: cyan;">{filename}</span>
+</div>
+<ul>
+  <li style="color: magenta;" on:dblclick={() => update('..')}>..</li>
+  {#each dirs as dir}
+    {#if !filter || filter.length < 3 || dir.includes(filter)}
+      <li style="color: yellow;" on:dblclick={() => update(dir)}>{dir}</li>
+    {/if}
+  {/each}
+  {#each files as file}
+    {#if !filter || filter.length < 3 || file.includes(filter)}
+      <li><label><input type="radio" name="old" value={file} on:change={handleSelected}>{file}</label></li>
+    {/if}
+  {/each}
+</ul>
--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -1,90 +1,49 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { onMount } from 'svelte'
   import { goto } from '$app/navigation';
 
+  import Pane from '../../components/explorer/Pane.svelte'
   import { type DiffTab } from '../../types'
   import { pushToDiffTabsStore, updateDiffTabIndexStore } from '../../stores'
 
-  // todo
-  let currentDir = ''
-
   let filter = ''
-  let dirs: string[] = []
-  let files: string[] = []
 
-  async function update(dir: string) {
-    currentDir = currentDir ? `${currentDir}/${dir}` : ''
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    const ret = await invoke("list_dir", { currentDir: currentDir }) as {current_dir: string, dirs: string[], files: string[]}
-
-    currentDir = ret.current_dir
-    dirs = ret.dirs
-    files = ret.files
-  }
-
-  async function ready() {
-    await update('')
-  }
-
-  onMount(ready)
-
+  
   function compare() {
-    pushToDiffTabsStore(<DiffTab>{oldFilepath: oldSelected, newFilepath: newSelected})
+    const diffTab = <DiffTab>{oldFilepath: `${oldDir}/${oldFilename}`, newFilepath: `${newDir}/${newFilename}`}
+    pushToDiffTabsStore(diffTab)
     goto('/')
   }
 
-  let oldSelected: string | undefined
-  function handleOldSelected(event: Event & { currentTarget: EventTarget & HTMLInputElement; }) {
-    oldSelected = `${currentDir}/${event.currentTarget.value}`
+  let oldDir: string = ''
+  let oldFilename: string = ''
+  function handleOldSelected(event: CustomEvent) {
+    const {dir, filename} = event.detail as {dir: string, filename: string}
+    oldDir = dir
+    oldFilename = filename
   }
-  let newSelected: string | undefined
-  function handleNewSelected(event: Event & { currentTarget: EventTarget & HTMLInputElement; }) {
-    newSelected = `${currentDir}/${event.currentTarget.value}`
+  
+  let newDir: string = ''
+  let newFilename: string = ''
+  function handleNewSelected(event: CustomEvent) {
+    const {dir, filename} = event.detail as {dir: string, filename: string}
+    newDir = dir
+    newFilename = filename
   }
 </script>
 
 <h1>Explorer</h1>
 
 <input id="filter-input" placeholder="Filter" bind:value={filter} />
-<button on:click={compare} disabled={!oldSelected || !newSelected}>Compare</button>
+<button on:click={compare} disabled={!oldFilename || !newFilename}>Compare</button>
 
-<h3>Filepath: {currentDir}</h3>
-<div><!-- todo -->
-  old: {oldSelected},
-  new: {newSelected}
-</div>
-<ul>
-  <div class="wrapper">
-    <div class="explorer old">
-      <li style="color: cyan;" on:dblclick={() => update('..')}>..</li>
-      {#each dirs as dir}
-        {#if !filter || filter.length < 3 || dir.includes(filter)}
-          <li style="color: yellow;" on:dblclick={() => update(dir)}>{dir}</li>
-        {/if}
-      {/each}
-      {#each files as file}
-        {#if !filter || filter.length < 3 || file.includes(filter)}
-          <li><label><input type="radio" name="old" value={file} on:change={handleOldSelected}>{file}</label></li>
-        {/if}
-      {/each}
-    </div>
-    <div class="explorer new">
-      <li style="color: cyan;" on:dblclick={() => update('..')}>..</li>
-      {#each dirs as dir}
-        {#if !filter || filter.length < 3 || dir.includes(filter)}
-          <li style="color: yellow;" on:dblclick={() => update(dir)}>{dir}</li>
-        {/if}
-      {/each}
-      {#each files as file}
-        {#if !filter || filter.length < 3 || file.includes(filter)}
-          <li><label><input type="radio" name="new" value={file} on:change={handleNewSelected}>{file}</label></li>
-        {/if}
-      {/each}
-    </div>
+<div class="wrapper">
+  <div class="explorer old">
+    <Pane filter={filter} on:selectedChange={handleOldSelected} />
   </div>
-</ul>
+  <div class="explorer new">
+    <Pane filter={filter} on:selectedChange={handleNewSelected} />
+  </div>
+</div>
 
 <style>
   .wrapper {
