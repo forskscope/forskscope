9664727ea42b54b7ff58abcd1918294c04075747
nabbisen <nabbisen@scqr.net>
2025-02-11 22:11:05 +0900

    fix crash in windows due to file path str

--- src-tauri/src/core/file.rs
+++ src-tauri/src/core/file.rs
@@ -1,12 +1,21 @@
-use std::fs::{read, read_to_string};
+use std::{
+    fs::{read, read_to_string},
+    path::PathBuf,
+};
 
 pub fn filepath_content(filepath: &str) -> String {
-    match read_to_string(filepath) {
+    let filepath = PathBuf::from(filepath);
+    match read_to_string(&filepath) {
         Ok(x) => x,
         // todo
         Err(_) => {
-            let read_as_bytes = read(filepath)
-                .expect(format!("Failed to read as text file: {}", filepath).as_str());
+            let read_as_bytes = read(&filepath).expect(
+                format!(
+                    "Failed to read as text file: {}",
+                    &filepath.to_str().unwrap_or_default()
+                )
+                .as_str(),
+            );
             read_as_bytes
                 .iter()
                 .map(|x| format!("{} ", x.to_string()))
