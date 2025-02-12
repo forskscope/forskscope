d8f9f73bdfea85583d0032c5b0ac8be11113f039
nabbisen <nabbisen@scqr.net>
2025-02-12 12:29:38 +0000

    remove unnecessary casting (#37)
    * next ver candidate
    
    * remove unnecessary casting

--- src-tauri/src/core/file.rs
+++ src-tauri/src/core/file.rs
@@ -1,21 +1,12 @@
-use std::{
-    fs::{read, read_to_string},
-    path::PathBuf,
-};
+use std::fs::{read, read_to_string};
 
 pub fn filepath_content(filepath: &str) -> String {
-    let filepath = PathBuf::from(filepath);
-    match read_to_string(&filepath) {
+    match read_to_string(filepath) {
         Ok(x) => x,
         // todo
         Err(_) => {
-            let read_as_bytes = read(&filepath).expect(
-                format!(
-                    "Failed to read as text file: {}",
-                    &filepath.to_str().unwrap_or_default()
-                )
-                .as_str(),
-            );
+            let read_as_bytes = read(&filepath)
+                .expect(format!("Failed to read as text file: {}", filepath).as_str());
             read_as_bytes
                 .iter()
                 .map(|x| format!("{} ", x.to_string()))
