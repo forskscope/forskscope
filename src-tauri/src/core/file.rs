e3c3e8b188337f765a4feb038ddaec37b2c32d92
nabbisen <nabbisen@scqr.net>
2025-02-15 15:53:49 +0900

    media file treated as binary instead of text file

--- src-tauri/src/core/file.rs
+++ src-tauri/src/core/file.rs
@@ -1,4 +1,4 @@
-use std::fs::{read_to_string, File};
+use std::fs::File;
 use std::io::Read;
 
 use chardetng::EncodingDetector;
@@ -9,30 +9,12 @@ const UTF8_CHARSET: &str = "UTF-8";
 const NOT_TEXTFILE_CHARSET: &str = "(bytes array)";
 
 pub fn filepath_content(filepath: &str) -> ReadContent {
-    match read_to_string(filepath) {
-        Ok(x) => {
-            return ReadContent {
-                charset: UTF8_CHARSET.to_owned(),
-                content: x,
-            }
-        }
-        Err(_) => (),
-    };
-
     let mut file = File::open(filepath).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
 
-    let mut detector = EncodingDetector::new();
-    detector.feed(&buffer, true);
-    let encoding = detector.guess(None, false);
-    let (decoded, _, had_errors) = encoding.decode(&buffer);
-    if !had_errors {
-        ReadContent {
-            charset: encoding.name().to_owned(),
-            content: decoded.to_string(),
-        }
-    } else {
+    let is_binary = buffer.windows(2).any(|window| window[0] == 0x00);
+    if is_binary {
         const BYTES_ARRAY_ROW_LENGTH: usize = 16;
         let mut grid = String::new();
         for chunk in buffer.chunks(BYTES_ARRAY_ROW_LENGTH) {
@@ -41,9 +23,31 @@ pub fn filepath_content(filepath: &str) -> ReadContent {
             }
             grid.push_str("\n");
         }
-        ReadContent {
+        return ReadContent {
             charset: NOT_TEXTFILE_CHARSET.to_owned(),
             content: grid,
+        };
+    }
+
+    match std::str::from_utf8(&buffer) {
+        Ok(x) => {
+            return ReadContent {
+                charset: UTF8_CHARSET.to_owned(),
+                content: x.to_owned(),
+            }
         }
+        Err(_) => (),
+    }
+
+    let mut detector = EncodingDetector::new();
+    detector.feed(&buffer, true);
+    let encoding = detector.guess(None, false);
+    let (decoded, _, had_errors) = encoding.decode(&buffer);
+    if had_errors {
+        eprint!("not binary, not utf-8 text and not any other encoded text.")
+    }
+    ReadContent {
+        charset: encoding.name().to_owned(),
+        content: decoded.to_string(),
     }
 }
